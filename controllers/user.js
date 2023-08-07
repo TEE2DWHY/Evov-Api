2fbbb50939c8b086aaf2d33b1c910f29d703d460
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-08-07 02:51:24 -0700

    completed verify email endpoint

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,7 @@ const { StatusCodes } = require("http-status-codes");
 const jwt = require("jsonwebtoken");
 const { sendEmail, verifyEmailMessage } = require("../utils/email");
 
+// register new user
 const register = asyncWrapper(async (req, res) => {
   const { email } = req.body;
   if (!email) {
@@ -11,38 +12,28 @@ const register = asyncWrapper(async (req, res) => {
       msg: "Please provide email",
     });
   }
-
-  try {
-    const verificationToken = jwt.sign({ email }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_LIFETIME,
-    });
-
-    const newUser = await User.create({
-      email: email,
-    });
-
-    await sendEmail({
-      email: email,
-      subject: "VERIFY YOUR EMAIL - EVOV",
-      message: verifyEmailMessage(verificationToken),
-    });
-
-    res.status(StatusCodes.CREATED).json({
-      msg: "Registration successful. Please check your email to verify.",
-    });
-  } catch (err) {
-    console.error("Registration error:", err);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      msg: "An error occurred during registration. Please try again later.",
-    });
-  }
+  const verificationToken = jwt.sign({ email }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+  const newUser = await User.create({
+    email: email,
+  });
+  await sendEmail({
+    email: email,
+    subject: "VERIFY YOUR EMAIL - EVOV",
+    message: verifyEmailMessage(verificationToken),
+  });
+  res.status(StatusCodes.CREATED).json({
+    msg: "Registration successful. Please check your email to verify.",
+  });
 });
 
+// verify user email after sign up
 const verifyEmail = asyncWrapper(async (req, res) => {
   const { token } = req.query;
   if (!token) {
     res.status(StatusCodes.BAD_REQUEST).json({
-      msg: `token not found or expired.`,
+      msg: `Token not found.`,
     });
   }
   try {
